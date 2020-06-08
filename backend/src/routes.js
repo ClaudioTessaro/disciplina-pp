99e81114bafa04bd2143a2ebc822542be41b53bf
Claudio Tessaro <claudio0190@hotmail.com>
2020-06-08 15:44:53 -0300

    first commit

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessionController";
+import TipoProdutoController from "./app/controllers/TipoProdutoController";
+import ProdutoController from "./app/controllers/ProdutoController";
+import authMiddleware from "./app/middlewares/auth";
+
+const routes = new Router();
+
+routes.post("/users", UserController.createUser);
+routes.post("/sessions", SessionController.store);
+
+routes.use(authMiddleware);
+routes.get("/users", UserController.recuperar);
+
+routes.post("/tipoProduto", TipoProdutoController.cadastrarProduto);
+routes.get("/tipoProduto", TipoProdutoController.buscarTodos);
+routes.get("/tipoProduto/:id", TipoProdutoController.buscarTipoPorId);
+routes.put("/tipoProduto/:id", TipoProdutoController.atualizarTipoProduto);
+routes.delete("/tipoProduto/:id", TipoProdutoController.deletar);
+
+export default routes;
